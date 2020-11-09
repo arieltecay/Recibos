23231e9f5ceb886d0e009416a02d76b72b4e42f6
arieltecay <arieltecay@gmail.com>
2020-11-09 19:25:59 -0300

    Creacion de componentes y NavBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import AddMovements from './components/AddMovements'
+import Saldo from './components/Saldo'
+import NavBar from './components/NavBar'
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Ariel Tecay</h1>
+      <BrowserRouter>
+        <NavBar/>
+        <Switch>
+          <Route path='/saldo' component={Saldo} exact />
+          <Route path='/addMovements' component={AddMovements} exact />
+        </Switch>
+      </BrowserRouter>
+
     </div>
   );
 }
---
+++ client/src/components/Saldo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+export default function Saldo(){
+    return (
+        <div className='pt-5'>
+            <h1>Saldo</h1>
+            
+        </div>
+    )
+}
