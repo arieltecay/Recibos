cfedbe6656283fb72b5dc67ff5f94eb0b965ddc2
arieltecay <arieltecay@gmail.com>
2020-11-09 19:50:28 -0300

    Creacion de Saldo

--- client/src/components/Saldo.jsx
+++ client/src/components/Saldo.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
-export default function Saldo(){
+export default function Saldo() {
     return (
         <div className='pt-5'>
             <h1>Saldo</h1>
-            
+            <div className='table-responsive'>
+                <form>
+                    <table className='table'>
+                        <thead>
+                            <tr>
+                                <th>Agregar</th>
+                            </tr>
+                            <tr>
+                                <th>
+                                    <input
+                                        required
+                                        type="number"
+                                        min='0'
+                                    />
+                                </th>
+                            </tr>
+                            <tr>
+                                <th>
+                                    <button type='button' className='btn btn-success'>Add</button>
+                                </th>
+                            </tr>
+                        </thead>
+                    </table>
+                </form>
+            </div>
+
         </div>
     )
 }
